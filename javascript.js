f5438eed55399529ffa99c863bfbd040f5224d22
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-08-11 17:54:45 -0700

    - Added flex styling to position buttons for calc - Added tertiary background and border styling for buttons and display - Added initial add, subtract, multiply, and divide functions to js

---
+++ javascript.js
@@ -0,0 +1,35 @@
+function add(a,b){
+    if (!Number.isNaN(a) && !Number.isNaN(b)){
+        return a + b;
+    }
+    else{
+        console.log("Non number operand");
+    }
+}
+
+function subtract(a,b){
+    if (!Number.isNaN(a) && !Number.isNaN(b)){
+        return a - b;
+    }
+    else{
+        console.log("Non number operand");
+    }
+}
+
+function multiply(a,b){
+    if (!Number.isNaN(a) && !Number.isNaN(b)){
+        return a * b;
+    }
+    else{
+        console.log("Non number operand");
+    }
+}
+
+function divide(a,b){
+    if (!Number.isNaN(a) && !Number.isNaN(b)){
+        return a / b;
+    }
+    else{
+        console.log("Non number operand");
+    }
+}

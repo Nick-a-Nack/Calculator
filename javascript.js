c37d0fb674e4cdb388b8b5ccaa0a6ed06c5c4e3b
Nicholas Cox <118236483+Nick-a-Nack@users.noreply.github.com>
2023-08-13 19:03:38 -0700

    - Added operator function - Added global variables to hold inputA, inputB, and operand - Added logic to decide where to store variables in updateDisplay function

--- javascript.js
+++ javascript.js
@@ -1,3 +1,7 @@
+let inputA = null;
+let inputB = null;
+let operator = null;
+
 function add(a,b){
     if (!Number.isNaN(a) && !Number.isNaN(b)){
         return a + b;
@@ -42,5 +46,45 @@ function updateDisplay(input){
     else{
         temp.textContent = temp.textContent + input;
     }
+//Checks for where values should be stored. First test is for the first digit of first operand
+//Second test is if you are still inputting the first operand before giving it an operator
+//Third test is to update second operand if an operator has been selected already
+    if (inputA == null){
+        inputA = Number.parseFloat(temp.textContent);
+    }
+    else if (inputA != null && operator == null){
+        inputA = Number.parseFloat(temp.textContent);
+    }
+    else if (operator != null){
+        inputB = Number.parseFloat(temp.textContent);
+    }
     
+}
+
+function operate(inputA, inputB, operator){
+    if (Number.isNaN(inputA) || Number.isNaN(inputB)){
+        console.log("Non number operand");
+    }
+    else{
+        if (operator == "+"){
+            return add(inputA, inputB);
+        }
+        else if (operator == "-"){
+            return subtract(inputA, inputB);
+        }
+        else if (operator == "*"){
+            return multiply(inputA, inputB);
+        }
+        else if (operator == "/"){
+            return divide(inputA, inputB);
+        }
+        else{
+            console.log("Something went wrong here ..");
+            return -1;
+        }
+    }
+    
+    if (operator == '+'){
+        return add(inputA, inputB);
+    }
 }
